46e3ac1f1e29cb569e00b9156d6adf068d23f3b3
KacperBar99 <UL0243393@edu.uni.lodz.pl>
2021-12-16 21:33:02 +0100

    Poprawniejsza implementacja strzelania po sieci
    Działa teraz lepiej, ale nadal można trochę się pobawić położeniem punktu strzału

--- projektzespol/Assets/Scripts/playerShooting.cs
+++ projektzespol/Assets/Scripts/playerShooting.cs
@@ -7,10 +7,11 @@ public class playerShooting : NetworkBehaviour
     public GameObject projectile;
     public weaponSwitch w_switch;
     public playerShooting player;
+    public Transform player2;
     
 
     public float shoot_f, upward_f, shoot_cd, cd_timer;
-
+    public GameObject attack;
     public Camera cam;
     public Transform attack_point;
     Vector3 target_point, direction;
@@ -83,6 +84,7 @@ public class playerShooting : NetworkBehaviour
     [ClientRpc]
     public void RpcShoot()
     {
+        /*
         ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
         target_point = ray.GetPoint(10);
 
@@ -91,6 +93,30 @@ public class playerShooting : NetworkBehaviour
         GameObject current_projectile = Instantiate(projectile, attack_point.position, Quaternion.identity);
         current_projectile.GetComponent<Rigidbody>().AddForce(direction * shoot_f, ForceMode.Impulse);
         current_projectile.GetComponent<Rigidbody>().AddForce(direction * upward_f, ForceMode.Impulse);
-        Debug.Log(w_switch.weapon_select);
+        Debug.Log(w_switch.weapon_select);*/
+        GameObject tmp = Instantiate(projectile, attack_point.position, attack_point.rotation);
+
+        float y_value;
+        y_value=attack.GetComponentInChildren<attack_point>().tmp;
+        
+        float x_value;
+        float z_value;
+        x_value = Mathf.Cos(y_value * Mathf.Deg2Rad)*tmp.transform.forward.x;
+        z_value = Mathf.Cos(y_value * Mathf.Deg2Rad)*tmp.transform.forward.z;
+
+
+        if (y_value <= 0) y_value = Mathf.Sin(y_value * Mathf.Deg2Rad) * -1;
+        else y_value = Mathf.Sin(y_value * Mathf.Deg2Rad) * -1;
+       // float multi = 1;
+       // if (Mathf.Abs(y_value) >= 45) multi = .5f;
+        //if (Mathf.Abs(y_value) >= 90) multi = .25f;
+
+        
+        direction = new Vector3(x_value,y_value,z_value);
+        tmp.GetComponent<Rigidbody>().velocity = direction*10;
+        Debug.Log(direction);
+        //tmp.GetComponent<Rigidbody>().AddForce(direction * shoot_f*5, ForceMode.Impulse);
+        //tmp.GetComponent<Rigidbody>().AddForce(direction * upward_f*5, ForceMode.Impulse);
+        
     }
 }
